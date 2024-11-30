4974bafb75d03a80481900c72266eba75b8e4b43
Meriem453 <mimi.04.05.2003@gmail.com>
2024-11-30 20:34:05 +0100

    error fixed

--- Baridi-Trust-Backend/app.js
+++ Baridi-Trust-Backend/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const DBConnect = require('./DB/connect')
-const authRouter = require('./routers/auth')
+const transactionRoute = require('./routers/transactionsRoute')
 const errorHandeler = require('./middlewares/error-handeler')
 
 const app = express()
@@ -12,8 +12,8 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
-app.use("/api/v1/auth",authRouter)
-  
+app.use("/",transactionRoute)
+
 
 //error handeler middleware
 app.use(errorHandeler)
