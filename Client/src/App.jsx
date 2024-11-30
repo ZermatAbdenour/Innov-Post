5e04e9a1252b07afbe3e1bfa07be11c6b8540389
whoamif <adjissifatimaamina@gmail.com>
2024-12-01 00:40:09 +0100

    up : red + auth : still have to do styling

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductPage from "./pages/ProductPage";
-
+import RedirectPage from "./pages/RedirectPage";
+import Auth from "./pages/Authentication"
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
       <Routes>
         {/* Route for the Product Page */}
         <Route path="/" element={<ProductPage />} />
-
+        <Route path="/RedirectPage" element={<RedirectPage />} />
+        <Route path="/Auth" element={<Auth />} />
       </Routes>
     </Router>
   );
--- Client/src/pages/ProductPage.jsx
+++ Client/src/pages/ProductPage.jsx
@@ -1,43 +1,44 @@
 import React from "react";
-import navbar from "../assets/navbar.svg"
-import corp from "../assets/corp.svg"
-import counter from "../assets/counter.svg"
+import navbar from "../assets/navbar.svg";
+import corp from "../assets/corp.svg";
+import counter from "../assets/counter.svg";
 
 const ProductPage = () => {
   return (
     <div className="font-sans text-gray-800">
       {/* Navbar */}
-      <header className="flex justify-between ">
-      <img
-            src={navbar} 
-            alt="Cool Blue Chair"
-            className="w-full h-auto "
-          />
+      <header className="flex justify-between bg-gray-100 shadow-md">
+        <img
+          src={navbar}
+          alt="Navbar"
+          className="w-full h-auto"
+        />
       </header>
 
-     
-    
       {/* Product Section */}
-      <main className="flex flex-col ">
-      <img 
-            src={corp}
-            alt="Cool Blue Chair"
-            className="px-24 py-16"
-          />
-          <div className="flex gap-12">
+      <main className="flex flex-col  py-16 space-y-8">
+        {/* Corporate Image */}
+        <img
+          src={corp}
+          alt="Corporate Image"
+          className="w-3/4 max-w-5xl ml-14"
+        />
+
+        {/* Counter and Button Section */}
+        <div className="flex items-center gap-8 ml-14">
+          {/* Counter Image */}
           <img
             src={counter}
-            alt="Cool Blue Chair"
-            className="h-14 w-52 ml-24 "
+            alt="Counter"
+            className="h-14 w-32"
           />
-          <button className="bg-yellow-500 hover:bg-yellow-700 font-bold text-lg text-white px-6 py-2 rounded-xl h-14 w-52">
-              Buy Now
-            </button>
-          </div>
-          
-         
+
+          {/* Buy Now Button */}
+          <button className="bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 font-bold text-base text-white px-8 py-2 rounded-xl shadow-md ">
+            Buy Now
+          </button>
+        </div>
       </main>
-      
     </div>
   );
 };
