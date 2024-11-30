1061f4b09f6ab033d24273bcfe8e3e62a4ef5f35
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2024-11-30 17:10:13 +0100

    test

--- Third-Party-Backend/routers/auth.js
+++ Third-Party-Backend/routers/auth.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const authentication = require('../middlewares/auth')
-const {login,register} = require('../controllers/user')
-const validateRequest = require('../middlewares/validate-request')
-const {registerSchema,loginSchema} = require('../schemas/userSchemas')
+const express = require("express");
+const authentication = require("../middlewares/auth");
+const { login, register } = require("../controllers/user");
+const validateRequest = require("../middlewares/validate-request");
+const { registerSchema, loginSchema } = require("../schemas/userSchemas");
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/login').post(validateRequest(loginSchema),login)
-router.route('/register').post(validateRequest(registerSchema),register)
-router.route('/').get(authentication,(req,res)=>{
-    res.json(req.user)
-})
-module.exports = router
+///
+
+router.route("/login").post(validateRequest(loginSchema), login);
+router.route("/register").post(validateRequest(registerSchema), register);
+router.route("/").get(authentication, (req, res) => {
+  res.json(req.user);
+});
+module.exports = router;
