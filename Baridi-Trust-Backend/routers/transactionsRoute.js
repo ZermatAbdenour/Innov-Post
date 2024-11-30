41ee42506e1d40a27738779fdfea778263888941
Meriem453 <mimi.04.05.2003@gmail.com>
2024-11-30 22:49:49 +0100

    reports almost done

--- Baridi-Trust-Backend/routers/transactionsRoute.js
+++ Baridi-Trust-Backend/routers/transactionsRoute.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const authentication = require('../middlewares/auth')
-const {sellerValidation, buyerValidation, cancelTransaction, issueValidation, reportIssue} = require("../controllers/transactions");
+const {sellerValidation, buyerValidation, cancelTransaction, issueValidation, reportIssue, getAllBuyerTransactions,
+    getAllSellerTransactions, getOneTransaction, getAllUsers
+} = require("../controllers/transactions");
 
 const router = express.Router()
+
+router.route('/users').get(getAllUsers)
+
 
 router.route('/sellerValidation').post(sellerValidation)
 router.route('/buyerValidation').post(buyerValidation)
 router.route('/cancelTransaction').post(cancelTransaction)
 router.route('/reportIssue').post(reportIssue)
+
+router.route('/buyer/all').get(getAllBuyerTransactions)
+router.route('/seller/all').get(getAllSellerTransactions)
+router.route('/transactionDetails').get(getOneTransaction)
 
 
 module.exports = router
