d329318ca6565478cbc2928760559dd5f421ec0c
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2024-12-01 02:44:26 +0100

    almost done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductPage from "./pages/ProductPage";
-
+import TransactionDetailsBuyer from "./pages/TransactionDetailsBuyer";
+import TransactionDetailsSeller from "./pages/TransactionDetailsSeller";
+import ReportTransaction from "./pages/ReportTransaction";
 
 const App = () => {
   return (
@@ -9,7 +11,9 @@ const App = () => {
       <Routes>
         {/* Route for the Product Page */}
         <Route path="/" element={<ProductPage />} />
-
+        <Route path="/transaction-buyer" element={<TransactionDetailsBuyer />} />
+        <Route path="/transaction-seller" element={<TransactionDetailsSeller />} />
+        <Route path="/report-transaction" element={<ReportTransaction />} />
       </Routes>
     </Router>
   );
--- Client/src/pages/ProductPage.jsx
+++ Client/src/pages/ProductPage.jsx
@@ -8,36 +8,36 @@ const ProductPage = () => {
     <div className="font-sans text-gray-800">
       {/* Navbar */}
       <header className="flex justify-between ">
-      <img
-            src={navbar} 
-            alt="Cool Blue Chair"
-            className="w-full h-auto "
-          />
+        <img
+          src={navbar}
+          alt="Cool Blue Chair"
+          className="w-full h-auto "
+        />
       </header>
 
-     
-    
+
+
       {/* Product Section */}
       <main className="flex flex-col ">
-      <img 
-            src={corp}
-            alt="Cool Blue Chair"
-            className="px-24 py-16"
-          />
-          <div className="flex gap-12">
+        <img
+          src={corp}
+          alt="Cool Blue Chair"
+          className="px-24 py-16"
+        />
+        <div className="flex gap-12">
           <img
             src={counter}
             alt="Cool Blue Chair"
             className="h-14 w-52 ml-24 "
           />
           <button className="bg-yellow-500 hover:bg-yellow-700 font-bold text-lg text-white px-6 py-2 rounded-xl h-14 w-52">
-              Buy Now
-            </button>
-          </div>
-          
-         
+            Buy Now
+          </button>
+        </div>
+
+
       </main>
-      
+
     </div>
   );
 };
