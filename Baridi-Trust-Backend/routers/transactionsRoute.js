022698f012513f1ab5b0c3cd43f2de5206504a79
Meriem453 <mimi.04.05.2003@gmail.com>
2024-12-01 09:19:56 +0100

    adding report schema

--- Baridi-Trust-Backend/routers/transactionsRoute.js
+++ Baridi-Trust-Backend/routers/transactionsRoute.js
@@ -3,6 +3,8 @@ const authentication = require('../middlewares/auth')
 const {sellerValidation, buyerValidation, cancelTransaction, reportIssue, getAllTransactions,
      getOneTransaction, getAllUsers
 } = require("../controllers/transactions");
+const validateRequest = require("../middlewares/validate-request");
+const {reportSchema} = require("../schemas/transactionSchemas");
 
 
 const router = express.Router()
@@ -13,7 +15,7 @@ router.route('/users').get(getAllUsers)
 router.route('/sellerValidation').post(authentication,sellerValidation)
 router.route('/buyerValidation').post(authentication,buyerValidation)
 router.route('/cancelTransaction').post(authentication,cancelTransaction)
-router.route('/reportIssue').post(authentication,reportIssue)
+router.route('/reportIssue').post(validateRequest(reportSchema),authentication,reportIssue)
 
 router.route('/').get(authentication,getAllTransactions)
 router.route('/:id').get(authentication,getOneTransaction)
